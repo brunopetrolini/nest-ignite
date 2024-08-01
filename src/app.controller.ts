c81b14c93be4d734e728b5023a1f65be48269552
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-08-01 16:00:24 -0300

    feat: adds prisma orm

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common'
-import { AppService } from './app.service'
 
-@Controller()
+import { PrismaService } from './prisma/prisma.service'
+
+@Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Get('/users')
+  public async getUsers() {
+    return await this.prismaService.user.findMany()
   }
 }
