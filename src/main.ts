a4d42e7eefd6bc86a4d94486968f787bca38d07b
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-08-01 17:11:45 -0300

    feat: adds environment validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 
-import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { Env } from './env'
+import { RootModule } from './root.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const app = await NestFactory.create(RootModule)
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+  const PORT = configService.get('PORT', { infer: true })
+  await app.listen(PORT)
 }
 bootstrap()
