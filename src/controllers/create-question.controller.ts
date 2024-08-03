f13257fcecc3954a6387cf0d417079ec8fc5b3ea
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-08-03 12:34:51 -0300

    feat: implements question creation

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,17 +1,55 @@
-import { Controller, HttpCode, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common'
 
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { TokenPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBody = z.infer<typeof createQuestionBodySchema>
+
+const payloadValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
 
 @Controller('/api')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor() {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Post('/questions')
   @HttpCode(201)
-  public async handle(@CurrentUser() user: TokenPayload) {
-    return { user_id: user.sub }
+  public async handle(
+    @Body(payloadValidationPipe) payload: CreateQuestionBody,
+    @CurrentUser() user: TokenPayload,
+  ) {
+    const { title, content } = payload
+    const userId = user.sub
+
+    const slug = this.convertToSlug(title)
+
+    const { id } = await this.prismaService.question.create({
+      data: {
+        title,
+        content,
+        authorId: userId,
+        slug,
+      },
+    })
+
+    return { id }
+  }
+
+  private convertToSlug(title: string) {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '') // Remove non-alpha-numeric characters except hyphens
+      .replace(/\s+/g, '-') // Replace white-spaces with hyphens
   }
 }
